ad2efc95963bbb90b0b8f9b0c07adabc441cd87a
harutiro <harutiro2027@outlook.jp>
2022-10-16 20:43:25 +0900

    update: 加速度と角速度の取得

--- app/src/main/java/net/harutiro/sensoraccelerationget/MainActivity.kt
+++ app/src/main/java/net/harutiro/sensoraccelerationget/MainActivity.kt
@@ -1,11 +1,84 @@
 package net.harutiro.sensoraccelerationget
 
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
+    private var textViewAccel: TextView? = null
+    private var textViewGyro: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get an instance of the SensorManager
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // Get an instance of the TextView
+        textViewAccel = findViewById(R.id.text_view_accel)
+        textViewGyro = findViewById(R.id.text_view_gyro)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        // Listenerの登録
+        val accel = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val gyro = sensorManager?.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
+        sensorManager?.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager?.registerListener(this, gyro, SensorManager.SENSOR_DELAY_UI);
+        //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+        //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME);
+        //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    // 解除するコードも入れる!
+    override fun onPause() {
+        super.onPause()
+        // Listenerを解除
+        sensorManager!!.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val sensorX = event.values[0]
+            val sensorY = event.values[1]
+            val sensorZ = event.values[2]
+            val strTmp =
+                    "加速度センサー\n " +
+                    "X: $sensorX \n" +
+                    "Y: $sensorY\n" +
+                    "Z: $sensorZ\n"
+
+
+            textViewAccel!!.text = strTmp
+        }
+
+        if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+            val sensorX = event.values[0]
+            val sensorY = event.values[1]
+            val sensorZ = event.values[2]
+            val strTmp =
+                        "ジャイロセンサー(角速度)\n " +
+                        "X: $sensorX \n" +
+                        "Y: $sensorY\n" +
+                        "Z: $sensorZ\n"
+
+            textViewGyro!!.text = strTmp
+        }
+
+    }
+
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
 }
